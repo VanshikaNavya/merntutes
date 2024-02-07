995bd8e5e51fc2a71561c129e16a1eb02973bf93
Vanshika <vanikanavya@gmail.com>
2024-02-08 04:02:45 +0530

    read file

--- NODE/file.js
+++ NODE/file.js
@@ -1,4 +1,14 @@
-// const fs = require("fs")
+// creating and writing to files through fs
+const fs = require("fs")
 
 // fs.writeFileSync("text.txt", "Jai Siya Ram")
 // fs.writeFile("vanshika.txt", "jai bajrangbali", (err)=>{})
+
+//const result = fs.readFileSync("vanshika.txt", "utf-8")
+//console.log(result)
+
+fs.readFile("vanshik.txt", "utf-8", (err, answer)=>{
+    if(err) console.log("Error")
+    else console.log(answer)
+})
+
